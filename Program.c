be3c0ef93189a80f92d40273fa08c42035376ba1
Hardik Soni <131765546+ItsHardikSoni@users.noreply.github.com>
2023-05-04 11:19:48 +0530

    Update and rename Triangle Valid or Not (IF  ELSE) to Program.c

--- Program.c
+++ Program.c
@@ -1,4 +1,8 @@
-/* Sum of Three Side of a triangle and check triangle is valid or not */
+// Write a program to check whether a triangle is valid or not, 
+// if three angles of the triangle are entered through the keyboard.
+// A triangle is valid if the sum of all the three angles is equal to 180 degrees.
+
+
 #include<stdio.h>
 #include<conio.h>
 int main()
